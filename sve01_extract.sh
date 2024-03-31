4d1451bee00e4fbdde20e2652972ee0932fec9f7
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-03-31 12:39:32 +0100

    Add parameters to extract script to specify area.

--- sve01_extract.sh
+++ sve01_extract.sh
@@ -20,80 +20,60 @@
 # ----------------------------------------------------------------------------
 # What's the file that we are interested in?
 #
-#file_prefix1=europe
-#file_page1=http://download.geofabrik.de/${file_prefix1}.html
-#file_url1=http://download.geofabrik.de/${file_prefix1}-latest.osm.pbf
+# The data file is downloaded in ~/data which allows it to be shared with data
+# files user by update_render.sh (raster maps) or garmin_map_etrex_03.sh 
+# (Garmin) if either of those are also installed.
 #
-#file_prefix1=britain-and-ireland
-#file_prefix1=great-britain
-#file_prefix1=ireland-and-northern-ireland
-#file_page1=http://download.geofabrik.de/europe/${file_prefix1}.html
-#file_url1=http://download.geofabrik.de/europe/${file_prefix1}-latest.osm.pbf
-#
-file_prefix1=england
-#file_prefix1=scotland
-#file_prefix1=wales
-file_page1=http://download.geofabrik.de/europe/great-britain/${file_prefix1}.html
-file_url1=http://download.geofabrik.de/europe/great-britain/${file_prefix1}-latest.osm.pbf
-#
-#file_prefix1=bedfordshire
-#file_prefix1=berkshire
-#file_prefix1=bristol
-#file_prefix1=buckinghamshire
-#file_prefix1=cambridgeshire
-#file_prefix1=cheshire
-#file_prefix1=cornwall
-#file_prefix1=cumbria
-#file_prefix1=derbyshire
-#file_prefix1=devon
-#file_prefix1=dorset
-#file_prefix1=durham
-#file_prefix1=east-sussex
-#file_prefix1=east-yorkshire-with-hull
-#file_prefix1=essex
-#file_prefix1=gloucestershire
-#file_prefix1=greater-london
-#file_prefix1=greater-manchester
-#file_prefix1=hampshire
-#file_prefix1=herefordshire
-#file_prefix1=hertfordshire
-#file_prefix1=isle-of-wight
-#file_prefix1=kent
-#file_prefix1=lancashire
-#file_prefix1=leicestershire
-#file_prefix1=lincolnshire
-#file_prefix1=merseyside
-#file_prefix1=norfolk
-#file_prefix1=north-yorkshire
-#file_prefix1=northamptonshire
-#file_prefix1=northumberland
-#file_prefix1=nottinghamshire
-#file_prefix1=oxfordshire
-#file_prefix1=rutland
-#file_prefix1=shropshire
-#file_prefix1=somerset
-#file_prefix1=south-yorkshire
-#file_prefix1=staffordshire
-#file_prefix1=suffolk
-#file_prefix1=surrey
-#file_prefix1=tyne-and-wear
-#file_prefix1=warwickshire
-#file_prefix1=west-midlands
-#file_prefix1=west-sussex
-#file_prefix1=west-yorkshire
-#file_prefix1=wiltshire
-#file_prefix1=worcestershire
-#file_page1=http://download.geofabrik.de/europe/great-britain/england/${file_prefix1}.html
-#file_url1=http://download.geofabrik.de/europe/great-britain/england/${file_prefix1}-latest.osm.pbf
-#
-#file_prefix1=new-york
-#file_prefix1=oregon
-#file_page1=http://download.geofabrik.de/north-america/us/${file_prefix1}.html
-#file_url1=http://download.geofabrik.de/north-america/us/${file_prefix1}-latest.osm.pbf
-#
-#file_prefix1=argentina
-#file_page1=http://download.geofabrik.de/south-america/${file_prefix1}.html
-#file_url1=http://download.geofabrik.de/south-america/${file_prefix1}-latest.osm.pbf
+# Four parameters can be set, such as:
+# europe great-britain england north-yorkshire
+# ----------------------------------------------------------------------------
+if [ -z "$4" ]
+then
+    if [ -z "$3" ]
+    then
+	if [ -z "$2" ]
+	then
+	    if [ -z "$1" ]
+	    then
+		echo "1-4 arguments needed (continent, country, state, region).  No arguments passed - exiting"
+		exit 1
+	    else
+		# ----------------------------------------------------------------------
+		# Sensible options here might be "antarctica" or possibly "central-america".
+		# ----------------------------------------------------------------------
+		echo "1 argument passed - processing a continent"
+		file_prefix1=${1}
+		file_page1=http://download.geofabrik.de/${file_prefix1}.html
+		file_url1=http://download.geofabrik.de/${file_prefix1}-latest.osm.pbf
+	    fi
+	else
+	    # ----------------------------------------------------------------------
+	    # Sensible options here might be e.g. "europe" and "albania".
+	    # ----------------------------------------------------------------------
+	    echo "2 arguments passed - processing a continent and a country"
+	    file_prefix1=${2}
+	    file_page1=http://download.geofabrik.de/${1}/${file_prefix1}.html
+	    file_url1=http://download.geofabrik.de/${1}/${file_prefix1}-latest.osm.pbf
+	fi
+    else
+	# ----------------------------------------------------------------------
+	# Sensible options here might be e.g. "europe" "united-kingdom" and "england".
+	# ----------------------------------------------------------------------
+	echo "3 arguments passed - processing a continent, a country and a subregion"
+	file_prefix1=${3}
+	file_page1=http://download.geofabrik.de/${1}/${2}/${file_prefix1}.html
+	file_url1=http://download.geofabrik.de/${1}/${2}/${file_prefix1}-latest.osm.pbf
+    fi
+else
+    # ----------------------------------------------------------------------
+    # Sensible options here might be e.g. "europe" "united-kingdom", "england" and "bedfordshire"
+    # ----------------------------------------------------------------------
+    echo "3 arguments passed - processing a continent, a country and a subregion"
+    file_prefix1=${4}
+    file_page1=http://download.geofabrik.de/${1}/${2}/${3}/${file_prefix1}.html
+    file_url1=http://download.geofabrik.de/${1}/${2}/${3}/${file_prefix1}-latest.osm.pbf
+fi
+
 #
 # How much disk space are we currently using?
 #
