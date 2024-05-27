78ac5e0dc94f40b1ca5022e38969bf43be62b970
Andy Townsend <ajtown@h12.atownsend.org.uk>
2024-05-27 11:23:13 +0000

    Update lua for tilemaker 3 shared_lua unchanged; it has no tilemaker specific stuff in it.

--- resources/process-sve01.lua
+++ resources/process-sve01.lua
@@ -86,40 +86,40 @@ function way_function(way)
 -- The assignment of local variables is done by the example code, I'm guessing
 -- for reasons of speed.
 -- ----------------------------------------------------------------------------
-    local highway = fix_corridors( way:Find("highway"), way:Find("layer"), way:Find("level") )
+    local highway = fix_corridors( Find("highway"), Find("layer"), Find("level") )
 
 -- ----------------------------------------------------------------------------
 -- Consolidate some rare highway types into ones we can display.
 -- ----------------------------------------------------------------------------
-    highway = process_golf_tracks( highway, way:Find("golf") )
+    highway = process_golf_tracks( highway, Find("golf") )
 
 -- ------------------------------------------------------------------------------
 -- Which other tags do we need to look at to see if a highway way has a 
 -- sidewalk or a verge?
 -- ------------------------------------------------------------------------------
-    local sidewalk = way:Find("sidewalk")
-    local sidewalkCleft = way:Find("sidewalk:left")
-    local sidewalkCright = way:Find("sidewalk:right")
-    local sidewalkCboth = way:Find("sidewalk:both")
-    local footway = way:Find("footway")
-    local shoulder = way:Find("shoulder")
-    local hard_shoulder = way:Find("hard_shoulder")
-    local cycleway = way:Find("cycleway")
-    local segregated = way:Find("segregated")
+    local sidewalk = Find("sidewalk")
+    local sidewalkCleft = Find("sidewalk:left")
+    local sidewalkCright = Find("sidewalk:right")
+    local sidewalkCboth = Find("sidewalk:both")
+    local footway = Find("footway")
+    local shoulder = Find("shoulder")
+    local hard_shoulder = Find("hard_shoulder")
+    local cycleway = Find("cycleway")
+    local segregated = Find("segregated")
 
-    local verge = way:Find("verge")
+    local verge = Find("verge")
 
 -- ------------------------------------------------------------------------------
 -- Other tags
 -- ------------------------------------------------------------------------------
-    local waterway = way:Find("waterway")
-    local building = way:Find("building")
+    local waterway = Find("waterway")
+    local building = Find("building")
 
 -- ----------------------------------------------------------------------------
 -- highway processing
 -- ----------------------------------------------------------------------------
     if ( highway ~= "" ) then
-        way:Layer("transportation", false)
+        Layer("transportation", false)
 
 -- ----------------------------------------------------------------------------
 -- Some highway types are temporarily rewritten to "catch all"s of 
@@ -132,12 +132,12 @@ function way_function(way)
             ( highway == "bridleway" ) or
             ( highway == "cycleway"  ) or
             ( highway == "steps"     )) then
-            way:Attribute( "class", "pathnarrow" )
+            Attribute( "class", "pathnarrow" )
         else
 	    if ( highway == "track" ) then
-                way:Attribute( "class", "pathwide" )
+                Attribute( "class", "pathwide" )
             else
-                way:Attribute( "class", highway )
+                Attribute( "class", highway )
             end
         end
 
@@ -174,7 +174,7 @@ function way_function(way)
             ( cycleway == "segregated"      ) or
             ( segregated == "yes"           ) or
             ( segregated == "right"         )) then
-            way:Attribute("edge", "sidewalk")
+            Attribute("edge", "sidewalk")
         else
 -- ----------------------------------------------------------------------------
 -- If there is not a sidewalk but there is a verge, set "edge" to "verge"
@@ -184,7 +184,7 @@ function way_function(way)
                 ( verge == "separate" ) or
                 ( verge == "right"    ) or
                 ( verge == "yes"      )) then
-                way:Attribute("edge", "verge")
+                Attribute("edge", "verge")
             end
         end
     end
@@ -193,12 +193,12 @@ function way_function(way)
 -- Other processing - still to be added
 -- ----------------------------------------------------------------------------
     if waterway~="" then
-        way:Layer("waterway", false)
-        way:Attribute("class", waterway)
+        Layer("waterway", false)
+        Attribute("class", waterway)
     end
 
     if building~="" then
-        way:Layer("building", true)
+        Layer("building", true)
     end
 
     generic_after_function( way )
@@ -215,59 +215,59 @@ function generic_before_function( passed_obj )
 -- ----------------------------------------------------------------------------
 -- Invalid layer values - change them to something plausible.
 -- ----------------------------------------------------------------------------
-    passed_obj:Attribute( "layer", fix_invalid_layer_values( passed_obj:Find("layer"), passed_obj:Find("bridge"), passed_obj:Find("embankment") ))
+    Attribute( "layer", fix_invalid_layer_values( Find("layer"), Find("bridge"), Find("embankment") ))
 
 -- ----------------------------------------------------------------------------
 -- Treat "was:" as "disused:"
 -- ----------------------------------------------------------------------------
-   if (( passed_obj:Find("was:amenity")     ~= nil ) and
-       ( passed_obj:Find("disused:amenity") == nil )) then
-      passed_obj:Attribute( "disused:amenity", passed_obj:Find( "was:amenity" ))
+   if (( Find("was:amenity")     ~= nil ) and
+       ( Find("disused:amenity") == nil )) then
+      Attribute( "disused:amenity", Find( "was:amenity" ))
    end
 
-   if (( passed_obj:Find("was:waterway")     ~= nil ) and
-       ( passed_obj:Find("disused:waterway") == nil )) then
-      passed_obj:Attribute( "disused:waterway", passed_obj:Find( "was:waterway" ))
+   if (( Find("was:waterway")     ~= nil ) and
+       ( Find("disused:waterway") == nil )) then
+      Attribute( "disused:waterway", Find( "was:waterway" ))
    end
 
-   if (( passed_obj:Find("was:railway")     ~= nil ) and
-       ( passed_obj:Find("disused:railway") == nil )) then
-      passed_obj:Attribute( "disused:railway", passed_obj:Find( "was:railway" ))
+   if (( Find("was:railway")     ~= nil ) and
+       ( Find("disused:railway") == nil )) then
+      Attribute( "disused:railway", Find( "was:railway" ))
    end
 
-   if (( passed_obj:Find("was:aeroway")     ~= nil ) and
-       ( passed_obj:Find("disused:aeroway") == nil )) then
-      passed_obj:Attribute( "disused:aeroway", passed_obj:Find( "was:aeroway" ))
+   if (( Find("was:aeroway")     ~= nil ) and
+       ( Find("disused:aeroway") == nil )) then
+      Attribute( "disused:aeroway", Find( "was:aeroway" ))
    end
 
-   if (( passed_obj:Find("was:landuse")     ~= nil ) and
-       ( passed_obj:Find("disused:landuse") == nil )) then
-      passed_obj:Attribute( "disused:landuse", passed_obj:Find( "was:landuse" ))
+   if (( Find("was:landuse")     ~= nil ) and
+       ( Find("disused:landuse") == nil )) then
+      Attribute( "disused:landuse", Find( "was:landuse" ))
    end
 
-   if (( passed_obj:Find("was:shop")     ~= nil ) and
-       ( passed_obj:Find("disused:shop") == nil )) then
-      passed_obj:Attribute( "disused:shop", passed_obj:Find( "was:shop" ))
+   if (( Find("was:shop")     ~= nil ) and
+       ( Find("disused:shop") == nil )) then
+      Attribute( "disused:shop", Find( "was:shop" ))
    end
 
 -- ----------------------------------------------------------------------------
 -- Treat "closed:" as "disused:" in some cases too.
 -- ----------------------------------------------------------------------------
-   if (( passed_obj:Find("closed:amenity")  ~= nil ) and
-       ( passed_obj:Find("disused:amenity") == nil )) then
-      passed_obj:Attribute( "disused:amenity", passed_obj:Find( "closed:amenity" ))
+   if (( Find("closed:amenity")  ~= nil ) and
+       ( Find("disused:amenity") == nil )) then
+      Attribute( "disused:amenity", Find( "closed:amenity" ))
    end
 
-   if (( passed_obj:Find("closed:shop")  ~= nil ) and
-       ( passed_obj:Find("disused:shop") == nil )) then
-      passed_obj:Attribute( "disused:shop", passed_obj:Find( "closed:shop" ))
+   if (( Find("closed:shop")  ~= nil ) and
+       ( Find("disused:shop") == nil )) then
+      Attribute( "disused:shop", Find( "closed:shop" ))
    end
 
 -- ----------------------------------------------------------------------------
 -- Treat "status=abandoned" as "disused=yes"
 -- ----------------------------------------------------------------------------
-   if ( passed_obj:Find("status") == "abandoned" ) then
-      passed_obj:Attribute( "disused", "yes" )
+   if ( Find("status") == "abandoned" ) then
+      Attribute( "disused", "yes" )
    end
 
 -- ----------------------------------------------------------------------------
@@ -275,71 +275,71 @@ function generic_before_function( passed_obj )
 -- containing both.
 -- If "name" does not exist but "name:en" does, use that.
 -- ----------------------------------------------------------------------------
-   passed_obj:Attribute( "name", set_name_left_right_en( passed_obj:Find("name"), passed_obj:Find("name:left"), passed_obj:Find("name:right"), passed_obj:Find("name:en") ))
+   Attribute( "name", set_name_left_right_en( Find("name"), Find("name:left"), Find("name:right"), Find("name:en") ))
 
 -- ----------------------------------------------------------------------------
 -- Move refs to consider as "official" to official_ref
 -- ----------------------------------------------------------------------------
-   passed_obj:Attribute( "official_ref", set_official_ref( passed_obj:Find("official_ref"), passed_obj:Find("highway_authority_ref"), passed_obj:Find("highway_ref"), passed_obj:Find("admin_ref"), passed_obj:Find("admin:ref"), passed_obj:Find("loc_ref"), passed_obj:Find("ref") ))
+   Attribute( "official_ref", set_official_ref( Find("official_ref"), Find("highway_authority_ref"), Find("highway_ref"), Find("admin_ref"), Find("admin:ref"), Find("loc_ref"), Find("ref") ))
 
 -- ----------------------------------------------------------------------------
 -- "Sabristas" sometimes add dubious names to motorway junctions.  Don't show
 -- them if they're not signed.
 -- ----------------------------------------------------------------------------
-   passed_obj:Attribute( "name", suppress_unsigned_motorway_junctions( passed_obj:Find("name"), passed_obj:Find("highway"), passed_obj:Find("name:signed"), passed_obj:Find("name:absent"), passed_obj:Find("unsigned") ))
+   Attribute( "name", suppress_unsigned_motorway_junctions( Find("name"), Find("highway"), Find("name:signed"), Find("name:absent"), Find("unsigned") ))
 
 -- ----------------------------------------------------------------------------
 -- Move unsigned road refs to the name, in brackets.
 -- ----------------------------------------------------------------------------
-   passed_obj:Attribute( "name", suppress_unsigned_road_refs( passed_obj:Find("name"), passed_obj:Find("highway"), passed_obj:Find("name:signed"), passed_obj:Find("name:absent"), passed_obj:Find("official_ref"), passed_obj:Find("ref"), passed_obj:Find("ref:signed"), passed_obj:Find("unsigned") ))
+   Attribute( "name", suppress_unsigned_road_refs( Find("name"), Find("highway"), Find("name:signed"), Find("name:absent"), Find("official_ref"), Find("ref"), Find("ref:signed"), Find("unsigned") ))
 
 -- ----------------------------------------------------------------------------
 -- Handle place=islet as place=island
 -- Handle place=quarter
 -- Handle natural=cape etc. as place=locality if no other place tag.
 -- ----------------------------------------------------------------------------
-   passed_obj:Attribute( "place", consolidate_place( passed_obj:Find("place"), passed_obj:Find("natural") ))
+   Attribute( "place", consolidate_place( Find("place"), Find("natural") ))
 
 end -- generic_before_function()
 
 
 function generic_after_function( passed_obj )
-    local amenity  = passed_obj:Find("amenity")
-    local place = passed_obj:Find("place")
-    local shop = passed_obj:Find("shop")
-    local tourism  = passed_obj:Find("tourism")
+    local amenity  = Find("amenity")
+    local place = Find("place")
+    local shop = Find("shop")
+    local tourism  = Find("tourism")
 
     if ( amenity ~= "" ) then
-        passed_obj:LayerAsCentroid( "poi" )
-	passed_obj:Attribute( "class","amenity_" .. amenity )
-	passed_obj:Attribute( "name", passed_obj:Find( "name" ))
+        LayerAsCentroid( "poi" )
+	Attribute( "class","amenity_" .. amenity )
+	Attribute( "name", Find( "name" ))
     else
         if ( place ~= "" ) then
-            passed_obj:LayerAsCentroid( "place" )
-    	    passed_obj:Attribute( "name", passed_obj:Find( "name" ))
+            LayerAsCentroid( "place" )
+    	    Attribute( "name", Find( "name" ))
 
             if (( place == "country" ) or
                 ( place == "state"   )) then
-                passed_obj:MinZoom( 5 )
+                MinZoom( 5 )
             else
                 if ( place == "city" ) then
-                    passed_obj:MinZoom( 5 )
+                    MinZoom( 5 )
                 else
                     if ( place == "town" ) then
-                        passed_obj:MinZoom( 8 )
+                        MinZoom( 8 )
                     else
                         if (( place == "suburb"  ) or
                             ( place == "village" )) then
-                            passed_obj:MinZoom( 11 )
+                            MinZoom( 11 )
                         else
                             if (( place == "hamlet"            ) or
                                 ( place == "locality"          ) or
                                 ( place == "neighbourhood"     ) or
                                 ( place == "isolated_dwelling" ) or
                                 ( place == "farm"              )) then
-                                passed_obj:MinZoom( 13 )
+                                MinZoom( 13 )
                             else
-                                passed_obj:MinZoom( 14 )
+                                MinZoom( 14 )
                             end -- hamlet
                         end -- suburb
                     end -- town
@@ -347,14 +347,14 @@ function generic_after_function( passed_obj )
             end --country
 	else -- place
             if ( shop ~= "" ) then
-                passed_obj:LayerAsCentroid( "poi" )
-    	        passed_obj:Attribute( "class","shop_" .. shop )
-    	        passed_obj:Attribute( "name", passed_obj:Find( "name" ))
+                LayerAsCentroid( "poi" )
+    	        Attribute( "class","shop_" .. shop )
+    	        Attribute( "name", Find( "name" ))
 	    else
                 if ( tourism ~= "" ) then
-                    passed_obj:LayerAsCentroid( "poi" )
-            	passed_obj:Attribute( "class","tourism_" .. tourism )
-            	passed_obj:Attribute( "name", passed_obj:Find( "name" ))
+                    LayerAsCentroid( "poi" )
+            	Attribute( "class","tourism_" .. tourism )
+            	Attribute( "name", Find( "name" ))
                 end -- tourism
             end -- shop
         end -- place
