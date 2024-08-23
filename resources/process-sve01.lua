d238a8f4575fa0ada1de820263475fa9e86548f4
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-08-24 00:46:42 +0100

    Changed generic_before to use a lua table

--- resources/process-sve01.lua
+++ resources/process-sve01.lua
@@ -53,11 +53,16 @@ end
 -- the "move OSM tags to vector layers"
 -- ------------------------------------------------------------------------------
 function node_function()
-    generic_before_function()
+    local nodet = {}
+    nodet.layer = Find("layer")
+    nodet.bridge = Find("bridge")
+    nodet.embankment = Find("embankment")
+
+    generic_before_function( nodet )
 
 -- No node-specific code yet
 
-    generic_after_function()
+    generic_after_function( nodet )
 end -- node_function()
 
 -- ------------------------------------------------------------------------------
@@ -74,7 +79,61 @@ end -- node_function()
 -- the "move OSM tags to vector layers"
 -- ------------------------------------------------------------------------------
 function way_function()
-    generic_before_function()
+    local wayt = {}
+    wayt.layer = Find("layer")
+    wayt.bridge = Find("bridge")
+    wayt.embankment = Find("embankment")
+    wayt.highway = Find("highway")
+    wayt.level = Find("level")
+    wayt.golf = Find("golf")
+    wayt.sidewalk = Find("sidewalk")
+    wayt.sidewalkCleft = Find("sidewalk:left")
+    wayt.sidewalkCright = Find("sidewalk:right")
+    wayt.sidewalkCboth = Find("sidewalk:both")
+    wayt.footway = Find("footway")
+    wayt.shoulder = Find("shoulder")
+    wayt.hard_shoulder = Find("hard_shoulder")
+    wayt.cycleway = Find("cycleway")
+    wayt.segregated = Find("segregated")
+    wayt.verge = Find("verge")
+    wayt.waterway = Find("waterway")
+    wayt.building = Find("building")
+    wayt.natural = Find("natural")
+    wayt.wasCamenity = Find("was:amenity")
+    wayt.closedCamenity = Find("closed:amenity")
+    wayt.disusedCamenity = Find("disused:amenity")
+    wayt.wasCwaterway = Find("was:waterway")
+    wayt.disusedCwaterway = Find("disused:waterway")
+    wayt.wasCrailway = Find("was:railway")
+    wayt.disusedCrailway = Find("disused:railway")
+    wayt.wasCaeroway = Find("was:aeroway")
+    wayt.disusedCaeroway = Find("disused:aeroway")
+    wayt.wasClanduse = Find("was:landuse")
+    wayt.disusedClanduse = Find("disused:landuse")
+    wayt.wasCshop = Find("was:shop")
+    wayt.closedCshop = Find("closed:shop")
+    wayt.disusedCshop = Find("disused:shop")
+    wayt.status = Find("status")
+    wayt.disused = Find("disused")
+    wayt.name = Find("name")
+    wayt.nameCleft = Find("name:left")
+    wayt.nameCright = Find("name:right")
+    wayt.nameCen = Find("name:en")
+    wayt.nameCsigned = Find("name:signed")
+    wayt.nameCabsent = Find("name:absent")
+    wayt.official_ref = Find("official_ref")
+    wayt.highway_authority_ref = Find("highway_authority_ref")
+    wayt.highway_ref = Find("highway_ref")
+    wayt.admin_ref = Find("admin_ref")
+    wayt.adminCref = Find("admin:ref")
+    wayt.loc_ref = Find("loc_ref")
+    wayt.ref = Find("ref")
+    wayt.refCsigned = Find("ref:signed")
+    wayt.unsigned = Find("unsigned")
+    wayt.place = Find("place")
+    wayt.surface = Find("surface")
+
+    generic_before_function( wayt )
 
 -- ----------------------------------------------------------------------------
 -- Before processing footways, turn certain corridors into footways
@@ -86,39 +145,17 @@ function way_function()
 -- The assignment of local variables is done by the example code, I'm guessing
 -- for reasons of speed.
 -- ----------------------------------------------------------------------------
-    local highway = fix_corridors( Find("highway"), Find("layer"), Find("level") )
+    wayt.highway = fix_corridors( wayt.highway, wayt.layer, wayt.level )
 
 -- ----------------------------------------------------------------------------
 -- Consolidate some rare highway types into ones we can display.
 -- ----------------------------------------------------------------------------
-    highway = process_golf_tracks( highway, Find("golf") )
-
--- ------------------------------------------------------------------------------
--- Which other tags do we need to look at to see if a highway way has a 
--- sidewalk or a verge?
--- ------------------------------------------------------------------------------
-    local sidewalk = Find("sidewalk")
-    local sidewalkCleft = Find("sidewalk:left")
-    local sidewalkCright = Find("sidewalk:right")
-    local sidewalkCboth = Find("sidewalk:both")
-    local footway = Find("footway")
-    local shoulder = Find("shoulder")
-    local hard_shoulder = Find("hard_shoulder")
-    local cycleway = Find("cycleway")
-    local segregated = Find("segregated")
-
-    local verge = Find("verge")
-
--- ------------------------------------------------------------------------------
--- Other tags
--- ------------------------------------------------------------------------------
-    local waterway = Find("waterway")
-    local building = Find("building")
+    wayt.highway = process_golf_tracks( wayt.highway, wayt.golf )
 
 -- ----------------------------------------------------------------------------
 -- highway processing
 -- ----------------------------------------------------------------------------
-    if ( highway ~= "" ) then
+    if ( wayt.highway ~= "" ) then
         Layer("transportation", false)
 
 -- ----------------------------------------------------------------------------
@@ -127,63 +164,63 @@ function way_function()
 -- Others are written through as the OSM highway type.
 -- For roads, sidewalk / verge information is written to an "edge" value.
 -- ----------------------------------------------------------------------------
-        if (( highway == "path"      ) or
-            ( highway == "footway"   ) or
-            ( highway == "bridleway" ) or
-            ( highway == "cycleway"  ) or
-            ( highway == "steps"     )) then
+        if (( wayt.highway == "path"      ) or
+            ( wayt.highway == "footway"   ) or
+            ( wayt.highway == "bridleway" ) or
+            ( wayt.highway == "cycleway"  ) or
+            ( wayt.highway == "steps"     )) then
             Attribute( "class", "pathnarrow" )
         else
-	    if ( highway == "track" ) then
+	    if ( wayt.highway == "track" ) then
                 Attribute( "class", "pathwide" )
             else
-                Attribute( "class", highway )
+                Attribute( "class", wayt.highway )
             end
         end
 
 -- ----------------------------------------------------------------------------
 -- If there is a sidewalk, set "edge" to "sidewalk"
 -- ----------------------------------------------------------------------------
-        if (( sidewalk == "both"            ) or 
-            ( sidewalk == "left"            ) or 
-            ( sidewalk == "mapped"          ) or 
-            ( sidewalk == "separate"        ) or 
-            ( sidewalk == "right"           ) or 
-            ( sidewalk == "shared"          ) or 
-            ( sidewalk == "yes"             ) or
-            ( sidewalkCboth == "separate"   ) or 
-            ( sidewalkCboth == "yes"        ) or
-            ( sidewalkCleft == "separate"   ) or 
-            ( sidewalkCleft == "segregated" ) or
-            ( sidewalkCleft == "yes"        ) or
-            ( sidewalkCright == "separate"  ) or 
-            ( sidewalkCright == "yes"       ) or
-            ( footway  == "separate"        ) or 
-            ( footway  == "yes"             ) or
-            ( shoulder == "both"            ) or
-            ( shoulder == "left"            ) or 
-            ( shoulder == "right"           ) or 
-            ( shoulder == "yes"             ) or
-            ( hard_shoulder == "yes"        ) or
-            ( cycleway == "track"           ) or
-            ( cycleway == "opposite_track"  ) or
-            ( cycleway == "yes"             ) or
-            ( cycleway == "separate"        ) or
-            ( cycleway == "sidewalk"        ) or
-            ( cycleway == "sidepath"        ) or
-            ( cycleway == "segregated"      ) or
-            ( segregated == "yes"           ) or
-            ( segregated == "right"         )) then
+        if (( wayt.sidewalk == "both"            ) or 
+            ( wayt.sidewalk == "left"            ) or 
+            ( wayt.sidewalk == "mapped"          ) or 
+            ( wayt.sidewalk == "separate"        ) or 
+            ( wayt.sidewalk == "right"           ) or 
+            ( wayt.sidewalk == "shared"          ) or 
+            ( wayt.sidewalk == "yes"             ) or
+            ( wayt.sidewalkCboth == "separate"   ) or 
+            ( wayt.sidewalkCboth == "yes"        ) or
+            ( wayt.sidewalkCleft == "separate"   ) or 
+            ( wayt.sidewalkCleft == "segregated" ) or
+            ( wayt.sidewalkCleft == "yes"        ) or
+            ( wayt.sidewalkCright == "separate"  ) or 
+            ( wayt.sidewalkCright == "yes"       ) or
+            ( wayt.footway  == "separate"        ) or 
+            ( wayt.footway  == "yes"             ) or
+            ( wayt.shoulder == "both"            ) or
+            ( wayt.shoulder == "left"            ) or 
+            ( wayt.shoulder == "right"           ) or 
+            ( wayt.shoulder == "yes"             ) or
+            ( wayt.hard_shoulder == "yes"        ) or
+            ( wayt.cycleway == "track"           ) or
+            ( wayt.cycleway == "opposite_track"  ) or
+            ( wayt.cycleway == "yes"             ) or
+            ( wayt.cycleway == "separate"        ) or
+            ( wayt.cycleway == "sidewalk"        ) or
+            ( wayt.cycleway == "sidepath"        ) or
+            ( wayt.cycleway == "segregated"      ) or
+            ( wayt.segregated == "yes"           ) or
+            ( wayt.segregated == "right"         )) then
             Attribute("edge", "sidewalk")
         else
 -- ----------------------------------------------------------------------------
 -- If there is not a sidewalk but there is a verge, set "edge" to "verge"
 -- ----------------------------------------------------------------------------
-            if (( verge == "both"     ) or
-                ( verge == "left"     ) or
-                ( verge == "separate" ) or
-                ( verge == "right"    ) or
-                ( verge == "yes"      )) then
+            if (( wayt.verge == "both"     ) or
+                ( wayt.verge == "left"     ) or
+                ( wayt.verge == "separate" ) or
+                ( wayt.verge == "right"    ) or
+                ( wayt.verge == "yes"      )) then
                 Attribute("edge", "verge")
             end
         end
@@ -192,16 +229,16 @@ function way_function()
 -- ----------------------------------------------------------------------------
 -- Other processing - still to be added
 -- ----------------------------------------------------------------------------
-    if waterway~="" then
+    if wayt.waterway~="" then
         Layer("waterway", false)
-        Attribute("class", waterway)
+        Attribute("class", wayt.waterway)
     end
 
-    if building~="" then
+    if wayt.building~="" then
         Layer("building", true)
     end
 
-    generic_after_function()
+    generic_after_function( wayt )
 end -- way_function()
 
 
@@ -211,63 +248,63 @@ end -- way_function()
 -- For methods available, see
 -- https://github.com/systemed/tilemaker/blob/master/docs/CONFIGURATION.md#principal-lua-functions
 -- ------------------------------------------------------------------------------
-function generic_before_function()
+function generic_before_function( passedt )
 -- ----------------------------------------------------------------------------
 -- Invalid layer values - change them to something plausible.
 -- ----------------------------------------------------------------------------
-    Attribute( "layer", fix_invalid_layer_values( Find("layer"), Find("bridge"), Find("embankment") ))
+    passedt.layer = fix_invalid_layer_values( passedt.layer, passedt.bridge, passedt.embankment )
 
 -- ----------------------------------------------------------------------------
 -- Treat "was:" as "disused:"
 -- ----------------------------------------------------------------------------
-   if (( Find("was:amenity")     ~= nil ) and
-       ( Find("disused:amenity") == nil )) then
-      Attribute( "disused:amenity", Find( "was:amenity" ))
+   if (( passedt.wasCamenity     ~= nil ) and
+       ( passedt.disusedCamenity == nil )) then
+      passedt.disusedCamenity = passedt.wasCamenity
    end
 
-   if (( Find("was:waterway")     ~= nil ) and
-       ( Find("disused:waterway") == nil )) then
-      Attribute( "disused:waterway", Find( "was:waterway" ))
+   if (( passedt.wasCwaterway     ~= nil ) and
+       ( passedt.disusedCwaterway == nil )) then
+      passedt.disusedCwaterway = passedt.wasCwaterway
    end
 
-   if (( Find("was:railway")     ~= nil ) and
-       ( Find("disused:railway") == nil )) then
-      Attribute( "disused:railway", Find( "was:railway" ))
+   if (( passedt.wasCrailway     ~= nil ) and
+       ( passedt.disusedCrailway == nil )) then
+      passedt.disusedCrailway = passedt.wasCrailway
    end
 
-   if (( Find("was:aeroway")     ~= nil ) and
-       ( Find("disused:aeroway") == nil )) then
-      Attribute( "disused:aeroway", Find( "was:aeroway" ))
+   if (( passedt.wasCaeroway     ~= nil ) and
+       ( passedt.disusedCaeroway == nil )) then
+      passedt.disusedCaeroway = passedt.wasCaeroway
    end
 
-   if (( Find("was:landuse")     ~= nil ) and
-       ( Find("disused:landuse") == nil )) then
-      Attribute( "disused:landuse", Find( "was:landuse" ))
+   if (( passedt.wasClanduse     ~= nil ) and
+       ( passedt.disusedClanduse == nil )) then
+      passedt.disusedClanduse = passedt.wasClanduse
    end
 
-   if (( Find("was:shop")     ~= nil ) and
-       ( Find("disused:shop") == nil )) then
-      Attribute( "disused:shop", Find( "was:shop" ))
+   if (( passedt.wasCshop     ~= nil ) and
+       ( passedt.disusedCshop == nil )) then
+      passedt.disusedCshop = passedt.wasCshop
    end
 
 -- ----------------------------------------------------------------------------
 -- Treat "closed:" as "disused:" in some cases too.
 -- ----------------------------------------------------------------------------
-   if (( Find("closed:amenity")  ~= nil ) and
-       ( Find("disused:amenity") == nil )) then
-      Attribute( "disused:amenity", Find( "closed:amenity" ))
+   if (( passedt.closedCamenity  ~= nil ) and
+       ( passedt.disusedCamenity == nil )) then
+      passedt.disusedCamenity = passedt.closedCamenity
    end
 
-   if (( Find("closed:shop")  ~= nil ) and
-       ( Find("disused:shop") == nil )) then
-      Attribute( "disused:shop", Find( "closed:shop" ))
+   if (( passedt.closedCshop  ~= nil ) and
+       ( passedt.disusedCshop == nil )) then
+      passedt.disusedCshop = passedt.closedCshop
    end
 
 -- ----------------------------------------------------------------------------
 -- Treat "status=abandoned" as "disused=yes"
 -- ----------------------------------------------------------------------------
-   if ( Find("status") == "abandoned" ) then
-      Attribute( "disused", "yes" )
+   if ( passedt.status == "abandoned" ) then
+      passedt.disused = "yes"
    end
 
 -- ----------------------------------------------------------------------------
@@ -275,55 +312,55 @@ function generic_before_function()
 -- containing both.
 -- If "name" does not exist but "name:en" does, use that.
 -- ----------------------------------------------------------------------------
-   Attribute( "name", set_name_left_right_en( Find("name"), Find("name:left"), Find("name:right"), Find("name:en") ))
+   passedt.name = set_name_left_right_en( passedt.name, passedt.nameCleft, passedt.nameCright, passedt.nameCen )
 
 -- ----------------------------------------------------------------------------
 -- Move refs to consider as "official" to official_ref
 -- ----------------------------------------------------------------------------
-   Attribute( "official_ref", set_official_ref( Find("official_ref"), Find("highway_authority_ref"), Find("highway_ref"), Find("admin_ref"), Find("admin:ref"), Find("loc_ref"), Find("ref") ))
+   passedt.official_ref = set_official_ref( passedt.official_ref, passedt.highway_authority_ref, passedt.highway_ref, passedt.admin_ref, passedt.adminCref, passedt.loc_ref, passedt.ref )
 
 -- ----------------------------------------------------------------------------
 -- "Sabristas" sometimes add dubious names to motorway junctions.  Don't show
 -- them if they're not signed.
 -- ----------------------------------------------------------------------------
-   Attribute( "name", suppress_unsigned_motorway_junctions( Find("name"), Find("highway"), Find("name:signed"), Find("name:absent"), Find("unsigned") ))
+   passedt.name = suppress_unsigned_motorway_junctions( passedt.name, passedt.highway, passedt.nameCsigned, passedt.nameCabsent, passedt.unsigned )
 
 -- ----------------------------------------------------------------------------
 -- Move unsigned road refs to the name, in brackets.
 -- ----------------------------------------------------------------------------
-   t = { Find("name"), Find("highway"), Find("name:signed"), Find("name:absent"), Find("official_ref"), Find("ref"), Find("ref:signed"), Find("unsigned") }
+   t = { passedt.name, passedt.highway, passedt.nameCsigned, passedt.nameCabsent, passedt.official_ref, passedt.ref, passedt.refCsigned, passedt.unsigned }
    suppress_unsigned_road_refs( t )
 
    if ( t[1] ~= nil ) then
-      Attribute( "name", t[1] )
+      passedt.name = t[1]
    end
 
    if ( t[2] ~= nil ) then
-      Attribute( "highway", t[2] )
+      passedt.highway = t[2]
    end
 
    if ( t[3] ~= nil ) then
-      Attribute( "name:signed", t[3] )
+      passedt.nameCsigned = t[3]
    end
 
    if ( t[4] ~= nil ) then
-      Attribute( "name:absent", t[4] )
+      passedt.nameCabsent = t[4]
    end
 
    if ( t[5] ~= nil ) then
-      Attribute( "official_ref", t[5] )
+      passedt.official_ref = t[5]
    end
 
    if ( t[6] ~= nil ) then
-      Attribute( "ref", t[6] )
+      passedt.ref = t[6]
    end
 
    if ( t[7] ~= nil ) then
-      Attribute( "ref:signed", t[7] )
+      passedt.refCsigned = t[7]
    end
 
    if ( t[8] ~= nil ) then
-      Attribute( "unsigned", t[8] )
+      passedt.unsigned = t[8]
    end
 
 -- ----------------------------------------------------------------------------
@@ -331,7 +368,20 @@ function generic_before_function()
 -- Handle place=quarter
 -- Handle natural=cape etc. as place=locality if no other place tag.
 -- ----------------------------------------------------------------------------
-   Attribute( "place", consolidate_place( Find("place"), Find("natural") ))
+   passedt.place = consolidate_place( passedt.place, passedt.natural )
+
+-- ----------------------------------------------------------------------------
+-- Show natural=bracken as scrub
+-- ----------------------------------------------------------------------------
+   if ( passedt.natural  == "bracken" ) then
+      passedt.natural = "scrub"
+   end
+
+   if ((  passedt.highway == "unclassified"  ) and
+       (( passedt.surface == "unpaved"      )  or 
+        ( passedt.surface == "gravel"       ))) then
+      passedt.highway = "unpaved"
+   end
 
 end -- generic_before_function()
 
