aade19fb941c701bcb3d7e0296f7f78552db5684
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-08-24 10:38:25 +0100

    Change "after" to use local variables Update taginfo

--- resources/process-sve01.lua
+++ resources/process-sve01.lua
@@ -54,9 +54,70 @@ end
 -- ------------------------------------------------------------------------------
 function node_function()
     local nodet = {}
+    nodet.amenity = Find("amenity")
+    nodet.place = Find("place")
+    nodet.shop = Find("shop")
+    nodet.wasCshop = Find("was:shop")
+    nodet.closedCshop = Find("closed:shop")
+    nodet.disusedCshop = Find("disused:shop")
+    nodet.tourism = Find("tourism")
     nodet.layer = Find("layer")
     nodet.bridge = Find("bridge")
     nodet.embankment = Find("embankment")
+    nodet.highway = Find("highway")
+    nodet.level = Find("level")
+    nodet.golf = Find("golf")
+    nodet.sidewalk = Find("sidewalk")
+    nodet.sidewalkCleft = Find("sidewalk:left")
+    nodet.sidewalkCright = Find("sidewalk:right")
+    nodet.sidewalkCboth = Find("sidewalk:both")
+    nodet.footway = Find("footway")
+    nodet.shoulder = Find("shoulder")
+    nodet.hard_shoulder = Find("hard_shoulder")
+    nodet.cycleway = Find("cycleway")
+    nodet.segregated = Find("segregated")
+    nodet.verge = Find("verge")
+    nodet.waterway = Find("waterway")
+    nodet.building = Find("building")
+    nodet.natural = Find("natural")
+    nodet.wasCamenity = Find("was:amenity")
+    nodet.closedCamenity = Find("closed:amenity")
+    nodet.disusedCamenity = Find("disused:amenity")
+    nodet.wasCwaterway = Find("was:waterway")
+    nodet.disusedCwaterway = Find("disused:waterway")
+    nodet.wasCrailway = Find("was:railway")
+    nodet.disusedCrailway = Find("disused:railway")
+    nodet.wasCaeroway = Find("was:aeroway")
+    nodet.disusedCaeroway = Find("disused:aeroway")
+    nodet.wasClanduse = Find("was:landuse")
+    nodet.disusedClanduse = Find("disused:landuse")
+    nodet.status = Find("status")
+    nodet.disused = Find("disused")
+    nodet.name = Find("name")
+    nodet.nameCleft = Find("name:left")
+    nodet.nameCright = Find("name:right")
+    nodet.nameCen = Find("name:en")
+    nodet.nameCsigned = Find("name:signed")
+    nodet.nameCabsent = Find("name:absent")
+    nodet.old_name = Find("old_name")
+    nodet.official_ref = Find("official_ref")
+    nodet.highway_authority_ref = Find("highway_authority_ref")
+    nodet.highway_ref = Find("highway_ref")
+    nodet.admin_ref = Find("admin_ref")
+    nodet.adminCref = Find("admin:ref")
+    nodet.loc_ref = Find("loc_ref")
+    nodet.ref = Find("ref")
+    nodet.refCsigned = Find("ref:signed")
+    nodet.unsigned = Find("unsigned")
+    nodet.surface = Find("surface")
+    nodet.visibility = Find("visibility")
+    nodet.trail_visibility = Find("trail_visibility")
+    nodet.footCphysical = Find("foot:physical")
+    nodet.overgrown = Find("overgrown")
+    nodet.obstacle = Find("obstacle")
+    nodet.informal = Find("informal")
+    nodet.width = Find("width")
+    nodet.est_width = Find("est_width")
 
     generic_before_function( nodet )
 
@@ -80,6 +141,13 @@ end -- node_function()
 -- ------------------------------------------------------------------------------
 function way_function()
     local wayt = {}
+    wayt.amenity = Find("amenity")
+    wayt.place = Find("place")
+    wayt.shop = Find("shop")
+    wayt.wasCshop = Find("was:shop")
+    wayt.closedCshop = Find("closed:shop")
+    wayt.disusedCshop = Find("disused:shop")
+    wayt.tourism = Find("tourism")
     wayt.layer = Find("layer")
     wayt.bridge = Find("bridge")
     wayt.embankment = Find("embankment")
@@ -110,9 +178,6 @@ function way_function()
     wayt.disusedCaeroway = Find("disused:aeroway")
     wayt.wasClanduse = Find("was:landuse")
     wayt.disusedClanduse = Find("disused:landuse")
-    wayt.wasCshop = Find("was:shop")
-    wayt.closedCshop = Find("closed:shop")
-    wayt.disusedCshop = Find("disused:shop")
     wayt.status = Find("status")
     wayt.disused = Find("disused")
     wayt.name = Find("name")
@@ -121,6 +186,7 @@ function way_function()
     wayt.nameCen = Find("name:en")
     wayt.nameCsigned = Find("name:signed")
     wayt.nameCabsent = Find("name:absent")
+    wayt.old_name = Find("old_name")
     wayt.official_ref = Find("official_ref")
     wayt.highway_authority_ref = Find("highway_authority_ref")
     wayt.highway_ref = Find("highway_ref")
@@ -130,8 +196,15 @@ function way_function()
     wayt.ref = Find("ref")
     wayt.refCsigned = Find("ref:signed")
     wayt.unsigned = Find("unsigned")
-    wayt.place = Find("place")
     wayt.surface = Find("surface")
+    wayt.visibility = Find("visibility")
+    wayt.trail_visibility = Find("trail_visibility")
+    wayt.footCphysical = Find("foot:physical")
+    wayt.overgrown = Find("overgrown")
+    wayt.obstacle = Find("obstacle")
+    wayt.informal = Find("informal")
+    wayt.width = Find("width")
+    wayt.est_width = Find("est_width")
 
     generic_before_function( wayt )
 
@@ -377,49 +450,144 @@ function generic_before_function( passedt )
       passedt.natural = "scrub"
    end
 
+-- ----------------------------------------------------------------------------
+-- Show natural=col as natural=saddle
+-- ----------------------------------------------------------------------------
+   if ( passedt.natural  == "col" ) then
+      passedt.natural = "saddle"
+   end
+ 
+-- ----------------------------------------------------------------------------
+-- Render old names on farmland etc.
+-- ----------------------------------------------------------------------------
+   if ((( passedt.landuse  == "farmland"       )  or
+        ( passedt.natural  == "grassland"      )  or
+        ( passedt.natural  == "scrub"          )) and
+       (  passedt.name     == nil               ) and
+       (  passedt.old_name ~= nil               )) then
+      passedt.name = "(" .. passedt.old_name .. ")"
+      passedt.old_name = nil
+   end
+
+-- ----------------------------------------------------------------------------
+-- If "visibility" is set but "trail_visibility" is not, use "visibility".
+-- ----------------------------------------------------------------------------
+   if (( passedt.visibility       ~= nil ) and
+       ( passedt.trail_visibility == nil )) then
+      passedt.trail_visibility = passedt.visibility
+   end
+
+-- ----------------------------------------------------------------------------
+-- Rationalise the various trail_visibility values into 3 sets
+-- (no value)    Implied good trail visibility.
+--               
+-- intermediate  Less trail visibility.  Shown with wider gaps in dotted line
+-- bad           Even less trail visibility.  Shown with wider gaps or not shown
+--               (depending on designation).
+--
+-- "trail_visibility=unknown" is treated as "good" since it's been mapped 
+-- from aerial imagery.  It's not explicitly referenced below.
+--
+-- "trail_visibility=low" is treated as "intermediate" based on looking at 
+-- the use in OSM.
+--
+-- Also treat "overgrown=yes" as "intermediate".  A discussion on talk-gb was
+-- largely inconclusive, but "overgrown" is the "most renderable" way to deal
+-- with things like this.  A later suggestion "foot:physical=no" is also 
+-- included.
+--
+-- "informal=yes" was less common in the UK (but is becoming more so).
+-- ----------------------------------------------------------------------------
+   if (( passedt.trail_visibility == "no"         )  or
+       ( passedt.trail_visibility == "none"       )  or
+       ( passedt.trail_visibility == "nil"        )  or
+       ( passedt.trail_visibility == "horrible"   )  or
+       ( passedt.trail_visibility == "very_bad"   )  or
+       ( passedt.trail_visibility == "bad"        )  or
+       ( passedt.trail_visibility == "poor"       )  or
+       ( passedt.footCphysical    == "no"         )) then
+      passedt.trail_visibility = "bad"
+   end
+
+   if ((  passedt.trail_visibility == "intermediate"  )  or
+       (  passedt.trail_visibility == "intermittent"  )  or
+       (  passedt.trail_visibility == "indistinct"    )  or
+       (  passedt.trail_visibility == "medium"        )  or
+       (  passedt.trail_visibility == "low"           )  or
+       (  passedt.overgrown        == "yes"           )  or
+       (  passedt.obstacle         == "vegetation"    )  or
+       (( passedt.trail_visibility == nil            )   and
+        ( passedt.informal         == "yes"          ))) then
+      passedt.trail_visibility = "intermediate"
+   end
+
+-- ----------------------------------------------------------------------------
+-- If we have an est_width but no width, use the est_width
+-- ----------------------------------------------------------------------------
+   if (( passedt.width     == nil  ) and
+       ( passedt.est_width ~= nil  )) then
+      passedt.width = passedt.est_width
+   end
+
+-- ----------------------------------------------------------------------------
+-- Here we apply the track grade rendering to road designations:
+--   unpaved roads                      unpaved
+--   narrow unclassigned_county_road    ucrnarrow
+--   wide unclassigned_county_road      ucrwide
+--   narrow BOAT			boatnarrow
+--   wide BOAT				boatwide
+--   narrow restricted byway		rbynarrow
+--   wide restricted byway		rbywide
+--
+-- prow_ref is appended in brackets if present.
+-- ----------------------------------------------------------------------------
    if ((  passedt.highway == "unclassified"  ) and
        (( passedt.surface == "unpaved"      )  or 
         ( passedt.surface == "gravel"       ))) then
       passedt.highway = "unpaved"
    end
 
+   if ((( passedt.highway == "residential"  )  or
+        ( passedt.highway == "service"      )) and
+       (( passedt.surface == "unpaved"      )  or 
+        ( passedt.surface == "gravel"       ))) then
+      passedt.highway = "track"
+   end
+
 end -- generic_before_function()
 
 
-function generic_after_function()
-    local amenity  = Find("amenity")
-    local place = Find("place")
-    local shop = Find("shop")
-    local tourism  = Find("tourism")
-
-    if ( amenity ~= "" ) then
+function generic_after_function( passedt )
+    if (( passedt.amenity ~= ""  ) and
+        ( passedt.amenity ~= nil )) then
         LayerAsCentroid( "poi" )
-	Attribute( "class","amenity_" .. amenity )
+	Attribute( "class","amenity_" .. passedt.amenity )
 	Attribute( "name", Find( "name" ))
     else
-        if ( place ~= "" ) then
+        if (( passedt.place ~= ""  ) and
+            ( passedt.place ~= nil )) then
             LayerAsCentroid( "place" )
     	    Attribute( "name", Find( "name" ))
 
-            if (( place == "country" ) or
-                ( place == "state"   )) then
+            if (( passedt.place == "country" ) or
+                ( passedt.place == "state"   )) then
                 MinZoom( 5 )
             else
-                if ( place == "city" ) then
+                if ( passedt.place == "city" ) then
                     MinZoom( 5 )
                 else
-                    if ( place == "town" ) then
+                    if ( passedt.place == "town" ) then
                         MinZoom( 8 )
                     else
-                        if (( place == "suburb"  ) or
-                            ( place == "village" )) then
+                        if (( passedt.place == "suburb"  ) or
+                            ( passedt.place == "village" )) then
                             MinZoom( 11 )
                         else
-                            if (( place == "hamlet"            ) or
-                                ( place == "locality"          ) or
-                                ( place == "neighbourhood"     ) or
-                                ( place == "isolated_dwelling" ) or
-                                ( place == "farm"              )) then
+                            if (( passedt.place == "hamlet"            ) or
+                                ( passedt.place == "locality"          ) or
+                                ( passedt.place == "neighbourhood"     ) or
+                                ( passedt.place == "isolated_dwelling" ) or
+                                ( passedt.place == "farm"              )) then
                                 MinZoom( 13 )
                             else
                                 MinZoom( 14 )
@@ -429,14 +597,16 @@ function generic_after_function()
                 end -- city
             end --country
 	else -- place
-            if ( shop ~= "" ) then
+            if (( passedt.shop ~= ""  ) and
+                ( passedt.shop ~= nil )) then
                 LayerAsCentroid( "poi" )
-    	        Attribute( "class","shop_" .. shop )
+    	        Attribute( "class","shop_" .. passedt.shop )
     	        Attribute( "name", Find( "name" ))
 	    else
-                if ( tourism ~= "" ) then
+                if (( passedt.tourism ~= ""  ) and
+                    ( passedt.tourism ~= nil )) then
                     LayerAsCentroid( "poi" )
-            	Attribute( "class","tourism_" .. tourism )
+            	Attribute( "class","tourism_" .. passedt.tourism )
             	Attribute( "name", Find( "name" ))
                 end -- tourism
             end -- shop
