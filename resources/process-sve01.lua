216630868ff814f84f0e4b4e32e2a11d69c84e01
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-10-03 02:12:25 +0100

    Added amenity=courthouse to extract

--- resources/process-sve01.lua
+++ resources/process-sve01.lua
@@ -6808,7 +6808,8 @@ function render_amenity_land1( passedt )
             ( passedt.amenity == "fuel_w"                  ) or
             ( passedt.amenity == "charging_station"        ) or
             ( passedt.amenity == "embassy"                 ) or
-            ( passedt.amenity == "library"                 )) then
+            ( passedt.amenity == "library"                 ) or
+            ( passedt.amenity == "courthouse"              )) then
             Layer( "land1", true )
             Attribute( "class", "amenity_" .. passedt.amenity )
             Attribute( "name", Find( "name" ) )
