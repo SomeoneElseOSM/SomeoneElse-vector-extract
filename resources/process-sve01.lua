b5a0f6240677883278322da582ac7eab94ac328a
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2025-06-08 14:54:47 +0100

    Spanish cuisine

--- resources/process-sve01.lua
+++ resources/process-sve01.lua
@@ -2414,6 +2414,7 @@ function render_amenity_land1( passedt )
                         ( passedt.amenity == "restaurant_caribbean"       ) or
                         ( passedt.amenity == "restaurant_lebanese"        ) or
                         ( passedt.amenity == "restaurant_dessert"         ) or
+                        ( passedt.amenity == "restaurant_spanish"         ) or
                         ( passedt.amenity == "fast_food"                  ) or
                         ( passedt.amenity == "fast_food_burger"           ) or
                         ( passedt.amenity == "fast_food_chicken"          ) or
@@ -2435,6 +2436,7 @@ function render_amenity_land1( passedt )
                         ( passedt.amenity == "fast_food_caribbean"        ) or
                         ( passedt.amenity == "fast_food_lebanese"         ) or
                         ( passedt.amenity == "fast_food_dessert"          ) or
+                        ( passedt.amenity == "fast_food_spanish"          ) or
                         ( passedt.amenity == "telephone"                  ) or
                         ( passedt.amenity == "boothtelephonered"          ) or
                         ( passedt.amenity == "boothtelephoneblack"        ) or
